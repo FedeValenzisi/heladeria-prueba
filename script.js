fc4c995fefabd44091ed50869521fa02a8550fdd
FedeValenzisi <fedevalenzisi@gmail.com>
2024-07-08 20:30:38 -0300

    Nosee

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var db = firebase.firestore();
+import { db } from "./index.js";
+import { getDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
-    db.collection("precios").doc("helado1kg").get().then(function(doc) {
-        if (doc.exists) {
-            document.getElementById("item1").innerText += " - $" + doc.data().precio;
+document.addEventListener("DOMContentLoaded", async function() {
+    try {
+        const helado1kgDoc = await getDoc(doc(db, "precios", "helado1kg"));
+        if (helado1kgDoc.exists()) {
+            document.getElementById("item1").innerText += " - $" + helado1kgDoc.data().precio;
+        } else {
+            console.log("No such document!");
         }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
-    });
 
-    db.collection("precios").doc("helado1/2kg").get().then(function(doc) {
-        if (doc.exists) {
-            document.getElementById("item2").innerText += " - $" + doc.data().precio;
+        const helado12kgDoc = await getDoc(doc(db, "precios", "helado1/2kg"));
+        if (helado12kgDoc.exists()) {
+            document.getElementById("item2").innerText += " - $" + helado12kgDoc.data().precio;
+        } else {
+            console.log("No such document!");
         }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
-    });
 
-    db.collection("precios").doc("helado1/4kg").get().then(function(doc) {
-        if (doc.exists) {
-            document.getElementById("item3").innerText += " - $" + doc.data().precio;
+        const helado14kgDoc = await getDoc(doc(db, "precios", "helado1/4kg"));
+        if (helado14kgDoc.exists()) {
+            document.getElementById("item3").innerText += " - $" + helado14kgDoc.data().precio;
+        } else {
+            console.log("No such document!");
         }
-    }).catch(function(error) {
-        console.log("Error getting document:", error);
-    });
+    } catch (error) {
+        console.log("Error getting documents: ", error);
+    }
 });
